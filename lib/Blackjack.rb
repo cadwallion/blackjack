df98603cea40c898d125a286f49cb4f4b63fdd19
Andrew Nordman <cadwallion@gmail.com>
2012-09-17 15:47:05 -0500

    Fixes outdate game state

--- lib/Blackjack.rb
+++ lib/Blackjack.rb
@@ -27,13 +27,14 @@ module Blackjack
 
     def draw
       @background.draw 0,0,0
-      @font.draw("Dealer", 275, 25, ZOrder::UI, 1.0, 1.0, 0xffffff00)
-      @font.draw("You", 290, 325, ZOrder::UI, 1.0, 1.0, 0xffffff00)
-      @font.draw("Balance: #{@player.balance}", 0, 450, ZOrder::UI, 1.0, 1.0, 0xffffff00)
-      @player.cards[0].draw 250, 350, ZOrder::Card
-      @player.cards[1].draw 275, 350, ZOrder::Card
-      @dealer.cards[0].draw 250, 50, ZOrder::Card
-      @dealer.cards[1].draw 275, 50, ZOrder::Card
+      if @game_in_progress
+        @font.draw("Dealer", 275, 25, ZOrder::UI, 1.0, 1.0, 0xffffff00)
+        @font.draw("You", 290, 325, ZOrder::UI, 1.0, 1.0, 0xffffff00)
+        @player.cards[0].draw 250, 350, ZOrder::Card
+        @player.cards[1].draw 275, 350, ZOrder::Card
+        @dealer.cards[0].draw 250, 50, ZOrder::Card
+        @dealer.cards[1].draw 275, 50, ZOrder::Card
+      end
     end
 
     def button_down id
